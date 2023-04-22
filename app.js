69ad3f8a512298b8bd1eab7efcee77c7cb5e1768
AkshatTiwari21 <43090549+AkshatTiwari21@users.noreply.github.com>
2023-04-22 20:18:21 +0000

    Added Seed data for future use

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-import express from "express";
+const express= require('express');
+const mongoose=require('mongoose');
+const path=require('path');
+const CampGroundSchema=require('./models/campground.js')
 
-const app= express()
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
-app.set('view engine','ejs')
+const db=mongoose.connection;
+
+db.on("error",console.error.bind(console,"connection error"));
+db.once("open",()=>{
+    console.log("Database Connected");
+})
+
+const onListening = () => {
+    console.log("Listening on PORT 5000");
+}
+const app= express();
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.listen(5000, onListening)
 
 app.get('/',(req,res)=>{
     res.render('home')
 })
 
-export default app
+app.get('/makeCampground',async (req,res)=>{
+    const camp= new CampGroundSchema({
+        title:'My New Campground',
+        description:'cheap camping!'
+    });
+    await camp.save();
+    res.send(camp)
+})
+
+app.get('/getTopCampGround',async (req,res)=>{
+    const camp= await CampGroundSchema.find({})
+    res.send(camp);
+})
